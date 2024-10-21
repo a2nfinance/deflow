717ede423717eb28ef4910c2aad60537199d5b56
a2nfinance <levi@a2n.finance>
2024-10-21 20:54:07 +0700

    update Bull queue for publish asset and start compute

--- app/src/queue/index.ts
+++ app/src/queue/index.ts
@@ -1,34 +1,53 @@
 import Queue from "bull";
 import connect from "@/database/connect";
-const assetQueue = new Queue("asset");
-const startComputeQueue = new Queue("compute");
-const dbConnect = connect({});
-assetQueue.process(async (job, done) => {
+import { start } from "@/oceancli";
+import Job, { JOB_STATES } from "@/database/models/job";
+import dotenv from "dotenv";
+dotenv.config();
+const REDIS_SERVER = process.env.REDIS_SERVER;
+const assetQueue = new Queue("asset", `redis://${REDIS_SERVER}:6379`);
+const startComputeQueue = new Queue("compute", `redis://${REDIS_SERVER}:6379`);
+assetQueue.process(async (job) => {
+    
+    const {nodeUrl, args, jobId} = job.data;
     try {
-        // Update DB here
-        const data = job.data;
-
+        console.log("Start Job");
+        await connect({});
         // process here
-
-        // update database here
-
+        let result = await start(nodeUrl, args);
+        if (result.success) {
+            let jobObj = await Job.findOneAndUpdate({ _id: jobId}, {state: JOB_STATES.FINISHED, result: {ddoId: result.assetId}});
+            console.log("Success to execute job:", jobObj);
+        } else {
+            let jobObj = await Job.findOneAndUpdate({ _id: jobId}, {state: JOB_STATES.FAILED, result: {message: result.message}});
+            console.log("Failed to execute job:", jobObj)
+        }
+        
     } catch(e) {
-        // update DB here
+        let jobObj = await Job.findOneAndUpdate({ _id: jobId}, {state: JOB_STATES.FAILED, result: {message: e.message}});
+        console.log("Failed to execute job:", jobObj)
     }
 
 })
 
-startComputeQueue.process( async (job, done) => {
+startComputeQueue.process( async (job) => {
+    const {nodeUrl, args, jobId} = job.data;
     try {
-        // Update DB here
-        const data = job.data;
-
+        console.log("Start Job");
+        await connect({});
         // process here
-
-        // update database here
-
+        let result = await start(nodeUrl, args);
+        if (result.success) {
+            let jobObj = await Job.findOneAndUpdate({ _id: jobId}, {state: JOB_STATES.FINISHED, result: result.result});
+            console.log("Success to execute job:", jobObj);
+        } else {
+            let jobObj = await Job.findOneAndUpdate({ _id: jobId}, {state: JOB_STATES.FAILED, result: {message: result.message}});
+            console.log("Failed to execute job:", jobObj)
+        }
+        
     } catch(e) {
-        // update DB here
+        console.log("Fail to execute Job: ", e);
+        await Job.findOneAndUpdate({ _id: jobId}, {state: JOB_STATES.FAILED, result: {message: e.message}});
     }
 })
 
